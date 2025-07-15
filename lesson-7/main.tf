a430a06fb549469a248ba7ea4e726c0e6489e1ce
Mykola Podopryhora <m.podopryhora@gmail.com>
2025-07-15 18:19:09 +0300

    Add EKRTerraform modules;  configure ECR; create Helm charts

---
+++ lesson-7/main.tf
@@ -0,0 +1,32 @@
+module "s3_backend" {
+  source = "./modules/s3-backend"                # Шлях до модуля
+  bucket_name = "podopryhora-goit-neoversity-tf-state-bucket"  # Ім'я S3-бакета
+  table_name  = "terraform-locks"                # Ім'я DynamoDB
+}
+
+# Підключаємо модуль для VPC
+module "vpc" {
+  source              = "./modules/vpc"           # Шлях до модуля VPC
+  vpc_cidr_block      = "10.0.0.0/16"             # CIDR блок для VPC
+  public_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]        # Публічні підмережі
+  private_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]         # Приватні підмережі
+  availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]            # Зони доступності
+  vpc_name            = "goit-vpc"              # Ім'я VPC
+}
+
+# Підключаємо модуль ECR
+module "ecr" {
+  source      = "./modules/ecr"
+  ecr_name    = "django-app"
+  scan_on_push = true
+}
+
+module "eks" {
+  source          = "./modules/eks"          
+  cluster_name    = "eks-cluster-demo"            # Назва кластера
+  subnet_ids      = module.vpc.public_subnets     # ID підмереж
+  instance_type   = "t3.small"                    # Тип інстансів
+  desired_size    = 1                             # Бажана кількість нодів
+  max_size        = 2                             # Максимальна кількість нодів
+  min_size        = 1                             # Мінімальна кількість нодів
+}
