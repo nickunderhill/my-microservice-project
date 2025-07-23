4f0ed90b26a124339eba80f78a7f509652d340b8
Mykola Podopryhora <m.podopryhora@gmail.com>
2025-07-23 08:42:41 +0300

    configure argocd

--- modules/argo_cd/argo_cd.tf
+++ modules/argo_cd/argo_cd.tf
@@ -2,6 +2,8 @@ resource "helm_release" "argo_cd" {
   name       = var.name
   namespace  = var.namespace
   repository = "https://argoproj.github.io/argo-helm"
+  replace      = true
+  force_update = true
   chart      = "argo-cd"
   version    = var.chart_version
 
